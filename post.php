838efaaf16562f396ee4bf87367032cf03873d88
ifotn <rich@infrontofthenet.com>
2023-01-16 09:54:01 -0500

    lesson 2/1 - start of mediacon

---
+++ post.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1>Create a New Post</h1>
+    <form action="save-post.php" method="post">
+        <fieldset>
+            <label for="body">Body:</label>
+            <textarea name="body" id="body"></textarea>
+        </fieldset>
+        <button>Post</button>
+    </form>
+</body>
+</html>
